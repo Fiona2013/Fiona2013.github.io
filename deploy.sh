7717cb800d86c4c7cdf5364c7fd345166503ee86
zacksleo <zacksleo@gmail.com>
2018-09-21 23:29:08 +0800

    add deploy

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# haolianluo.github.io builder.
+# @author zacksleo <zacksleo@gmail.com>
+#
+body='{
+"request": {
+    "message": "Update docs (triggered by Fiona2013/Fiona2013.github.io:docs).",
+    "branch":"hexo-theme-next"
+  }
+}'
+
+curl -s -X POST \
+  -H "Content-Type: application/json" \
+  -H "Accept: application/json" \
+  -H "Travis-API-Version: 3" \
+  -H "Authorization: token ${ACCESS_TOKEN}" \
+  -d "$body" \
+  https://api.travis-ci.org/repo/Fiona2013%2FFiona2013.github.io/requests
